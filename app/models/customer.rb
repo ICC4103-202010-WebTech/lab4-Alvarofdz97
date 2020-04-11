df24bc96d66a231ebd9ae9049e365cd77baffd4b
Alvarofdz97 <affernandez2@miuandes.cl>
2020-04-11 13:53:53 -0400

    Final

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
+  validates :name ,:lastname, :email, presence: true, email: true
   has_many :orders
   has_many :tickets, through: :orders
 end
--- app/models/ticket_type.rb
+++ app/models/ticket_type.rb
@@ -1,3 +1,4 @@
 class TicketType < ApplicationRecord
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000000000 }
   belongs_to :event
 end
